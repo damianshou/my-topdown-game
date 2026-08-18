a8e622fcf1dbe5084bf47c170b7323c7758ea230
damianshou <linya1991@qq.com>
2026-08-18 23:34:26 +0800

    feat(main menu): 为主菜单添加信号连接和基础回调框架
    1. 配置项目默认启动场景为主菜单
    2. 为所有主菜单和设置按钮添加 pressed 信号绑定
    3. 实现按钮切换界面的补间动画逻辑

--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -1,2 +1,41 @@
 extends Control
 class_name MainMenu
+@onready var main_buttons: Control = $MainButtons
+@onready var setting_buttons: Control = $SettingButtons
+
+# MainButtons 
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_setting_button_pressed() -> void:
+	# 动画位移
+	var tween := create_tween();
+	tween.tween_property(main_buttons,"global_position:y",350,0.2);
+	tween.tween_interval(0.1);
+	tween.tween_property(setting_buttons,"global_position:x",145,0.3);
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
+
+# SettingsButtons
+
+func _on_music_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_sfx_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_window_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	# 动画位移
+	var tween := create_tween();
+	tween.tween_property(setting_buttons,"global_position:x",558,0.3);
+	tween.tween_interval(0.1);
+	tween.tween_property(main_buttons,"global_position:y",115,0.2);
